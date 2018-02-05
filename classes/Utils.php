aa92970d37a31d364a8782c969370ba1b32f26d9
gostefan <gostefan@192.168.1.21>
2018-02-05 22:48:24 +0100

    Add a utility clean function.

---
+++ classes/Utils.php
@@ -0,0 +1,16 @@
+<?php
+class Utils {
+	private static $goodChars = "abcdefghijklmnopqrstuvwxyz";
+	public static function cleanName($name) {
+		$length = strlen($name);
+		$result = "";
+		for ($i = 0; $i < $length; $i++) {
+			if (strpos(self::$goodChars, $name[$i]) !== false)
+				$result .= $name[$i];
+			else
+				$result .= '_';
+		}
+		return $result;
+	}
+}
+?>
