989518edc3f85ceb405058cc6860823c732ea7ff
gostefan <gostefan@192.168.1.21>
2018-02-23 00:21:49 +0100

    Allow slashes in site paths.
    This is later used for images.

--- classes/Utils.php
+++ classes/Utils.php
@@ -1,6 +1,6 @@
 <?php
 class Utils {
-	private static $goodChars = "abcdefghijklmnopqrstuvwxyz";
+	private static $goodChars = "abcdefghijklmnopqrstuvwxyz/";
 	public static function cleanName($name) {
 		$length = strlen($name);
 		$result = "";
