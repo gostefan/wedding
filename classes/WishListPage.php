6a2154c57dfdc13e5aeebe89c8d25924ba92f50c
gostefan <gostefan@192.168.1.21>
2018-03-11 23:45:15 +0100

    Enhance Wish List Page.

--- classes/WishListPage.php
+++ classes/WishListPage.php
@@ -17,13 +17,13 @@ class WishListPage {
 			$this->categories[] = array("id" => $id, "name" => $name, "imageName" => $imageName);
 	}
 	
-	private const WISH_SELECT = "SELECT `name`, `description`, `imageName`, `category` from `wishes` ORDER BY `order` ASC";
+	private const WISH_SELECT = "SELECT `id`, `name`, `description`, `imageName`, `category`, `price`, SUM(`amount`) from `wishes` w LEFT JOIN `presents` p ON w.`id` = p.`wishid` GROUP BY `id` ORDER BY `order` ASC";
 	private function fetchWishes($db) {
 		$statement = $db->prepare(self::WISH_SELECT);
 		$statement->execute();
-		$statement->bind_result($name, $descr, $imageName, $category);
+		$statement->bind_result($id, $name, $descr, $imageName, $category, $price, $amount);
 		while($statement->fetch())
-			$this->wishes[$category][] = array("name" => $name, "descr" => $descr, "imageName" => $imageName);
+			$this->wishes[$category][] = array("id" => $id, "name" => $name, "descr" => $descr, "imageName" => $imageName, "price" => $price, "amount" => $amount);
 	}
 	
 	public function hasHeader() {
@@ -42,25 +42,32 @@ class WishListPage {
 		</div>
 <?php
 		foreach ($this->categories as $category) {
-		$wishesInCategory = $this->wishes[$category["id"]];
-		$totalWishes = count($wishesInCategory);
+			$wishesInCategory = $this->wishes[$category["id"]];
+			$totalWishes = count($wishesInCategory);
 ?>		<div class="staticImage" style="background-image: url(<?php echo ImageRequest::getPath($category['imageName']); ?>);">
 			<h2><?php echo $category['name']?></h2>
 			<div class="wishes">
-<?php 	$currentWish = 0;
-		for ($col = 0; $col < 2; $col++) { ?>
+<?php 		$currentWish = 0;
+			for ($col = 0; $col < 2; $col++) { ?>
 				<div class="wishColumn">
-<?php 		$columnLimit = $col == 0 ? ceil($totalWishes / 2.) : $totalWishes;
-			for (; $currentWish < $columnLimit; $currentWish++) { 
-				$wish = $wishesInCategory[$currentWish]; ?>
+<?php 			$columnLimit = $col == 0 ? ceil($totalWishes / 2.) : $totalWishes;
+				for (; $currentWish < $columnLimit; $currentWish++) { 
+					$wish = $wishesInCategory[$currentWish];
+					$available = $wish["price"] - $wish["amount"];
+					$percent = $available / $wish["price"] * 100; ?>
 					<div class="wish">
 						<img src="<?php echo ImageRequest::getPath($wish['imageName']); ?>" />
 						<div>
-							<div>
+							<div class="table">
 								<div>
 									<h3><?php echo htmlentities(utf8_encode($wish['name']), ENT_SUBSTITUTE); ?></h3>
 									<p><?php echo htmlentities(utf8_encode($wish['descr']), ENT_SUBSTITUTE); ?></p>
+									<p>Wert: <?php echo $wish["price"]; ?> CHF</p>
+									<p><a class="button" href="./schenken?id=<?php echo $wish["id"]; ?>">Schenken</a></p>
 								</div>
+							</div>
+							<div class="percentBox">
+								<div class="percent" style="width: <?php echo $percent; ?>%;"></div>
 							</div>
 						</div>
 					</div>
